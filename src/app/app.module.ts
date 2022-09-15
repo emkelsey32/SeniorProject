11a122f104584efeda96900d476f1ca06d004220
s414-3502 <mvadlamani@ufl.edu>
2022-09-15 14:45:19 -0400

    landing page navbar component created and scss seperated from html in app and landing page navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LandingNavbarComponent } from './landing-navbar/landing-navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LandingNavbarComponent
   ],
   imports: [
     BrowserModule,
