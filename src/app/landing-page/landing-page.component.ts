fe8813d73b67e563d5e6bbdd2134e12d4bdae2c9
ekels <ekelsey32@yahoo.com>
2022-10-11 16:21:14 -0400

    added progress bar updates

--- src/app/landing-page/landing-page.component.ts
+++ src/app/landing-page/landing-page.component.ts
@@ -1,3 +1,4 @@
+import { style } from '@angular/animations';
 import { Component, OnInit } from '@angular/core';
 import { Tab } from 'bootstrap';
 import {getUsers} from '../../API';
@@ -8,17 +9,32 @@ import {getUsers} from '../../API';
   styleUrls: ['./landing-page.component.scss']
 })
 export class LandingPageComponent implements OnInit {
-
+  mod1Progress = "";
   constructor() { }
 
   ngOnInit(): void {
+    let user = JSON.parse(localStorage.getItem("token")!)
+    let mod1Array = user!.progress[0].module1.progress;
+    let numComplete = 0;
+    for(let i = 0; i < mod1Array.length; i++){
+      if(mod1Array[i] == 1){
+        numComplete += 1;
+      }
+    }
+    console.log(mod1Array.length)
+    let mod1Number = Math.round(((numComplete * 100) / mod1Array.length))
+    this.mod1Progress = mod1Number + "%"
   }
 
   async someFunc() {
     var something = await getUsers();
     console.log(await getUsers());
     console.log("Hi")
+
   }
   
+  mod1ProgressStyle = style({
+      width: this.mod1Progress
+  })
 }
 
