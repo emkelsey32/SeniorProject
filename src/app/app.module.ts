58d6344d594315a1b06dad77cfd097d25fa2b416
s414-3502 <mvadlamani@ufl.edu>
2022-10-20 11:57:58 -0400

    lazy loading added to landing page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeNavbarComponent } from './home-navbar/home-navbar.component';
-import { LandingPageComponent } from './landing-page/landing-page.component';
 import { RegistrationPageComponent } from './registration-page/registration-page.component';
 import { SignInPageComponent } from './sign-in-page/sign-in-page.component';
-import { HomePageComponent } from './home-page/home-page.component';
 import { ProfilePageComponent } from './profile-page/profile-page.component';
 import { TermsAndConditionsComponent } from './terms-and-conditions/terms-and-conditions.component';
 import { FormsModule }   from '@angular/forms';
@@ -19,10 +17,8 @@ import { AboutInternalPageComponent } from './about-internal-page/about-internal
   declarations: [
     AppComponent,
     HomeNavbarComponent,
-    LandingPageComponent,
     RegistrationPageComponent,
     SignInPageComponent,
-    HomePageComponent,
     ProfilePageComponent,
     TermsAndConditionsComponent,
     MainNavbarComponent,
