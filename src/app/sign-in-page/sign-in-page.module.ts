4e2f885e36c6e73b9698ca6213d1173614db97e8
s414-3502 <mvadlamani@ufl.edu>
2022-10-20 12:01:19 -0400

    lazy loading added to sign in page

--- src/app/sign-in-page/sign-in-page.module.ts
+++ src/app/sign-in-page/sign-in-page.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { SignInPageRoutingModule } from './sign-in-page-routing.module';
+import { SignInPageComponent } from './sign-in-page.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [SignInPageComponent],
   imports: [
     CommonModule,
     SignInPageRoutingModule
