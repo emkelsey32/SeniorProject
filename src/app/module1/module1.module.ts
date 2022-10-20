b708ae56bd3bba346a4d8f7671702268c68be91d
s414-3502 <mvadlamani@ufl.edu>
2022-10-20 12:17:17 -0400

    added main navbar import to module1 module declarations

--- src/app/module1/module1.module.ts
+++ src/app/module1/module1.module.ts
@@ -5,12 +5,13 @@ import { Module1RoutingModule } from './module1-routing.module';
 import { ModuleOnePageOneComponent } from './module-one-page-one/module-one-page-one.component';
 import { ModuleOnePageTwoComponent } from './module-one-page-two/module-one-page-two.component';
 import { ModuleOnePracticalComponent } from './module-one-practical/module-one-practical.component';
-
+import { MainNavbarComponent } from '../main-navbar/main-navbar.component';
 @NgModule({
   declarations: [
     ModuleOnePageOneComponent,
     ModuleOnePageTwoComponent,
-    ModuleOnePracticalComponent
+    ModuleOnePracticalComponent,
+    MainNavbarComponent
   ],
   imports: [
     CommonModule,
